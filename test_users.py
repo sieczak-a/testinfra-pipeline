ca926010087a5ef2750a106d1208d2e7efc70a3c
r3dl0ck <76887338+r3dl0ck@users.noreply.github.com>
2021-01-15 09:31:16 +0100

    Update test_users.py

--- test_users.py
+++ test_users.py
@@ -4,9 +4,9 @@ def test_docker_group(host):
 
 
 def test_nickfury(host):
-    nickfury = host.user('vagrant')
+    nickfury = host.user('nickfury')
     assert nickfury.exists
 
-    nickfury_gids = host.user('vagrant').gids
+    nickfury_gids = host.user('nickfury').gids
     docker_gid = host.group("docker").gid
     assert docker_gid in nickfury_gids
